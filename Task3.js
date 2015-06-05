743626bb5c7e25b90f151c87588edb263c3d5f29
nolitha <nolithagrula@gmail.com>
2015-06-05 15:02:17 +0200

    Task3

---
+++ Task3.js
@@ -0,0 +1,6 @@
+		function button() {
+			document.getElementById("square").style.backgroundColor="green";
+		}
+		function button2() {
+			document.getElementById("square").style.backgroundColor="red";
+		}
